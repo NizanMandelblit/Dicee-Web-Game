82d136fa898ec9ecccc896e1ce0925dd2f42cab7
nizan <nizanm91@gmail.com>
2020-09-16 03:01:37 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 7);
+var randomNumber2 = Math.floor(Math.random() * 7);
+if (randomNumber1 == 0) {
+    randomNumber1++;
+}
+if (randomNumber2 == 0) {
+    randomNumber2++;
+}
+var string = "images/dice";
+string += randomNumber1.toString();
+string += ".png";
+document.querySelector(".img1").setAttribute("src", string);
+string = "images/dice";
+string += randomNumber2.toString();
+string += ".png";
+document.querySelector(".img2").setAttribute("src", string);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 wins!";
+} else {
+    if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").textContent = "Player 2 wins!";
+    } else {
+        document.querySelector("h1").textContent = "Draw!";
+    }
+}
+
+var test = 2;
